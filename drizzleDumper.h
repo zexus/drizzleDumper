c445d60b8fa562d09e0095d88c32dde068a6bc04
YipLau <sifooyee@gmail.com>
2019-03-25 19:59:47 +0800

    Bring up into android system build
    Change-Id: I14272cb52f5dcd0bd594cf9bda2ff2bf3654d0ec

--- drizzleDumper.h
+++ drizzleDumper.h
@@ -15,7 +15,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <linux/user.h>
+//#include <linux/user.h>
+#include <sys/user.h>
+#include <sys/reg.h>
 
 #ifdef HAVE_STDINT_H
 #include <stdint.h>    /* C99 */
